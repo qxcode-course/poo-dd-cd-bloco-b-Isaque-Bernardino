d88ecc52972a021cc6e37c6941a73f4a5c763153
Isaque-Bernardino <135618611+Isaque-Bernardino@users.noreply.github.com>
2025-10-08 23:28:29 +0000

    atv Roupa 80% realizada

---
+++ myrep/poo/roupa/py/draft.py
@@ -0,0 +1,26 @@
+class Camisa:
+    def __init__(self):
+        self.__tamanho = 0
+    def getTamanho(self):
+        return self.__tamanho
+    def setTamanho(self, letter: str):
+        if letter != "PP" and letter != "P" and letter != "M" and letter != "G" and letter != "GG" and letter != "XG":
+            print("fail: Valor inválido, tente PP, P, M, G, GG ou XG")
+    def __str__(self) -> str:
+        return f"size: ()"
+def main():
+    camisa = Camisa()
+    while True:
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(camisa)
+        elif args[0] == "size":
+            letter = str(args[1])
+            camisa.setTamanho(letter)
+
+main()
+        
