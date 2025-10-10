67868324db436b6b4b0e44949bf493662c7d3a2d
Isaque-Bernardino <135618611+Isaque-Bernardino@users.noreply.github.com>
2025-10-10 00:16:06 +0000

    atv Roupa 100% concluida e testada no tko

--- myrep/poo/roupa/py/draft.py
+++ myrep/poo/roupa/py/draft.py
@@ -1,13 +1,15 @@
 class Camisa:
     def __init__(self):
-        self.__tamanho = 0
+        self.__tamanho = ""
     def getTamanho(self):
         return self.__tamanho
     def setTamanho(self, letter: str):
         if letter != "PP" and letter != "P" and letter != "M" and letter != "G" and letter != "GG" and letter != "XG":
             print("fail: Valor inválido, tente PP, P, M, G, GG ou XG")
+        else:
+             self.__tamanho = letter
     def __str__(self) -> str:
-        return f"size: ()"
+        return f"size: ({self.__tamanho})"
 def main():
     camisa = Camisa()
     while True:
