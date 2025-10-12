410590f3e64f950b8e16a12e906182defd5bf320
Isaque-Bernardino <135618611+Isaque-Bernardino@users.noreply.github.com>
2025-10-12 19:22:12 +0000

    atv Relogio 100% realizada

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -12,21 +12,24 @@ class Watch:
     def setHour(self, hour: int):
         if hour < 0 or hour > 23:
             print("fail: hora invalida")
-            return
+            return False
         else:
             self.__hour = hour
+            return True
     def setMinute(self, minute: int):
         if minute < 0 or minute > 59:
             print("fail: minuto invalido")
-            return
+            return False
         else:
             self.__minute = minute
+            return True
     def setSecond(self, second: int):
         if second < 0 or second > 59:
             print("fail: segundo invalido")
-            return
+            return False
         else:
             self.__second = second
+            return True
     def nextSecond(self):
         if self.__hour != 23:
             self.__hour += 1
@@ -40,6 +43,12 @@ class Watch:
             self.__second += 1
         else:
             self.__second = 0
+    def resetHour(self):
+        self.__hour = 0
+    def resetMinute(self):
+        self.__minute = 0
+    def resetSecond(self):
+        self.__second = 0
     def __str__(self) -> str:
         return f"{self.__hour:02d}:{self.__minute:02d}:{self.__second:02d}"
 def main():
@@ -63,9 +72,12 @@ def main():
             hour = int(args[1])
             minute = int(args[2])
             second = int(args[3])
-            relogio.setHour(hour)
-            relogio.setMinute(minute)
-            relogio.setSecond(second)
+            if relogio.setHour(hour) == False:
+                relogio.resetHour()
+            if relogio.setMinute(minute) == False:
+                relogio.resetMinute()
+            if relogio.setSecond(second) == False:
+                relogio.resetSecond()
         if args[0] == "next":
             relogio.nextSecond()
 main()
